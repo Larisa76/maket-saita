fd2fa091bce3daba56f6844c87bbe542d8448bc6
Larisa76 <118682544+Larisa76@users.noreply.github.com>
2023-05-10 15:49:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ let navList = document.querySelector('.navigation')
 btn.addEventListener('click', () => {
     navList.classList.toggle('show-list')  
 })
+
+ const isOn = someCheckboxInput.checked
+
+
+  function serializeForm(formNode) {
+    const { elements } = formNode
+  
+    const data = Array.from(elements)
+      .map((element) => {
+        const { name, type } = element
+        const value = type === 'checkbox' ? element.checked : element.value
+  
+        return { name, value }
+      })
+      .filter((item) => !!item.name)
+    
+    console.log(data)
+  }
