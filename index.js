c57d0344127ec0c158da4565e7d31c6dd7af10d1
Larisa76 <118682544+Larisa76@users.noreply.github.com>
2023-06-15 22:37:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let btn = document.querySelector('.wrapper__btn') 
+let navList = document.querySelector('.navigation')
+
+
+btn.addEventListener('click', () => {
+    navList.classList.toggle('show-list')  
+})
+
+
+
+
+// const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+// fetch(url).then(data => {
+// return data.json();
+// }).then(data => {
+//   console(log)(data)
+// })
+
+
+
+
+
+            
+    
+
+  
